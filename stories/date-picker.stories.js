92fe221fdaa0764f994f0d95e9d8ec2a53111f8a
Erez Sobel <sobelerez@gmail.com>
2020-05-19 17:53:12 +0200

    docs: add stories for default short cut and date range

--- stories/date-picker.stories.js
+++ stories/date-picker.stories.js
@@ -7,3 +7,22 @@ storiesOf('VueDatePicker', module)
   .add('de locale', () => ({
     template: '<vue-date-picker locale="de" />'
   }))
+  .add('default short cut', () => ({
+    template: '<vue-date-picker default-short-cut="thisWeek" />'
+  }))
+  .add('date range', () => ({
+    template: '<vue-date-picker :date-range="dateRange" />',
+    data () {
+      return {
+        dateRange: { start: '2019-12-31T23:00:00.000Z', end: '2020-12-31T22:59:59.999Z', showDateText: true }
+      }
+    }
+  }))
+  .add('default short cut + date range', () => ({
+    template: '<vue-date-picker default-short-cut="thisWeek" :date-range="dateRange" />',
+    data () {
+      return {
+        dateRange: { start: '2019-12-31T23:00:00.000Z', end: '2020-12-31T22:59:59.999Z', showDateText: true }
+      }
+    }
+  }))
